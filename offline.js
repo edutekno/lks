7755eda28dd854dd7d57c2fbd02423d1cf2e2985
edutekno <54890481+edutekno@users.noreply.github.com>
2022-02-15 17:21:15 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1644920406,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/canvas.png",
+		"images/math2-sheet0.png",
+		"images/c-sheet0.png",
+		"images/bnext-sheet0.png",
+		"images/bpref-sheet0.png",
+		"images/cam-sheet0.png",
+		"images/download-sheet0.png",
+		"images/mathfrac-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/c0-sheet0.png",
+		"images/frame-sheet0.png",
+		"images/ct-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
